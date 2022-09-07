9890e018fdf79a51c6aee693dca589c1a2f05942
Ubuntu <ubuntu@ip-172-31-23-188.ec2.internal>
2022-09-07 05:14:04 +0000

    new cert.tf file

--- modules/ALB/cert.tf
+++ modules/ALB/cert.tf
@@ -1,21 +1,21 @@
 # The entire section create a certiface, public zone, and validate the certificate using DNS method
 
-# Create the certificate using a wildcard for all the domains created in project19.bossladies.click
-resource "aws_acm_certificate" "project_19_cert" {
+# Create the certificate using a wildcard for all the domains created in bossladies.click
+resource "aws_acm_certificate" "project_terraform" {
   domain_name       = "*.bossladies.click"
   validation_method = "DNS"
 }
 
 # calling the hosted zone
-data "aws_route53_zone" "project_19_zone" {
-  name         = "project19.bossladies.click"
+data "aws_route53_zone" "project_terraform" {
+  name         = "bossladies.click"
   private_zone = false
 }
 
 # selecting validation method
-resource "aws_route53_record" "project_19_record" {
+resource "aws_route53_record" "project_terraform" {
   for_each = {
-    for dvo in aws_acm_certificate.project_19_cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.project_terraform.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -27,19 +27,19 @@ resource "aws_route53_record" "project_19_record" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.project_19_zone.zone_id
+  zone_id         = data.aws_route53_zone.project_terraform.zone_id
 }
 
 # validate the certificate through DNS method
-resource "aws_acm_certificate_validation" "project_19_validation" {
-  certificate_arn         = aws_acm_certificate.project_19_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.project_19_record : record.fqdn]
+resource "aws_acm_certificate_validation" "project_terraform" {
+  certificate_arn         = aws_acm_certificate.project_terraform.arn
+  validation_record_fqdns = [for record in aws_route53_record.project_terraform : record.fqdn]
 }
 
 # create records for tooling
 resource "aws_route53_record" "tooling" {
-  zone_id = data.aws_route53_zone.project_19_zone.zone_id
-  name    = "tooling.project19.bossladies.click"
+  zone_id = data.aws_route53_zone.project_terraform.zone_id
+  name    = "tooling.bossladies.click"
   type    = "A"
 
   alias {
@@ -49,11 +49,10 @@ resource "aws_route53_record" "tooling" {
   }
 }
 
-
 # create records for wordpress
 resource "aws_route53_record" "wordpress" {
-  zone_id = data.aws_route53_zone.project_19_zone.zone_id
-  name    = "wordpress.project19.bossladies.click"
+  zone_id = data.aws_route53_zone.project_terraform.zone_id
+  name    = "wordpress.bossladies.click"
   type    = "A"
 
   alias {
