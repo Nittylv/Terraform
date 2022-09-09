513754e13070e8023cf2439a621d8be6be5ca890
Ubuntu <ubuntu@ip-172-31-88-127.ec2.internal>
2022-09-09 00:41:42 +0000

    health check

--- modules/ALB/alb.tf
+++ modules/ALB/alb.tf
@@ -1,28 +1,27 @@
 # ----------------------------
 #External Load balancer for reverse proxy nginx
-#-----------
+#---------------------------------
 
 resource "aws_lb" "ext-alb" {
-  name     = "ext-alb"
-  internal = false
+  name            = var.name
+  internal        = false
   security_groups = [var.public-sg]
-  
 
   subnets = [var.public-sbn-1,
-   var.public-sbn-2, ]
+  var.public-sbn-2, ]
 
-   tags = merge(
+  tags = merge(
     var.tags,
     {
-      Name = "ACS-ext-alb"
+      Name = var.name
     },
   )
 
-  ip_address_type    = "ipv4"
-  load_balancer_type = "application"
+  ip_address_type    = var.ip_address_type
+  load_balancer_type = var.load_balancer_type
 }
 
-
+#--- create a target group for the external load balancer
 resource "aws_lb_target_group" "nginx-tgt" {
   health_check {
     interval            = 10
@@ -39,6 +38,21 @@ resource "aws_lb_target_group" "nginx-tgt" {
   vpc_id      = var.vpc_id
 }
 
+#--- create a listener for the load balancer
+
+# resource "aws_lb_listener" "nginx-listner" {
+#   load_balancer_arn = aws_lb.ext-alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate_validation.benny.certificate_arn
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.nginx-tgt.arn
+#   }
+# }
+
+
 
 # ----------------------------
 #Internal Load Balancers for webservers
@@ -47,14 +61,13 @@ resource "aws_lb_target_group" "nginx-tgt" {
 resource "aws_lb" "ialb" {
   name     = "ialb"
   internal = true
-  security_groups = [
-    var.private-sg,
-  ]
+
+  security_groups = [var.private-sg]
 
   subnets = [var.private-sbn-1,
-   var.private-sbn-2,]
+  var.private-sbn-2, ]
 
-  tags = merge(
+    tags = merge(
     var.tags,
     {
       Name = "ACS-int-alb"
@@ -64,6 +77,7 @@ resource "aws_lb" "ialb" {
   ip_address_type    = var.ip_address_type
   load_balancer_type = var.load_balancer_type
 }
+
 
 # --- target group  for wordpress -------
 
@@ -77,12 +91,15 @@ resource "aws_lb_target_group" "wordpress-tgt" {
     unhealthy_threshold = 2
   }
 
-  name        = "wordpress-tgt"
-  port        = 443
+  name     = "wordpress-tgt"
+   port        = 443
   protocol    = "HTTPS"
   target_type = "instance"
   vpc_id      = var.vpc_id
-}
+  }
+
+
+
 
 # --- target group for tooling -------
 
@@ -96,7 +113,7 @@ resource "aws_lb_target_group" "tooling-tgt" {
     unhealthy_threshold = 2
   }
 
-  name        = "tooling-tgt"
+  name        = "david-tooling-tgt"
   port        = 443
   protocol    = "HTTPS"
   target_type = "instance"
@@ -106,34 +123,37 @@ resource "aws_lb_target_group" "tooling-tgt" {
 # For this aspect a single listener was created for the wordpress which is default,
 # A rule was created to route traffic to tooling when the host header changes
 
-resource "aws_lb_listener" "web-listener" {
-  load_balancer_arn = aws_lb.ialb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate_validation.benny.certificate_arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wordpress-tgt.arn
-  }
-}
+# resource "aws_lb_listener" "web-listener" {
+#   load_balancer_arn = aws_lb.ialb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate_validation.benny.certificate_arn
+
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.wordpress-tgt.arn
+#   }
+# }
 
 # listener rule for tooling target
 
-resource "aws_lb_listener_rule" "tooling-listener" {
-  listener_arn = aws_lb_listener.web-listener.arn
-  priority     = 99
+# resource "aws_lb_listener_rule" "tooling-listener" {
+#   listener_arn = aws_lb_listener.web-listener.arn
+#   priority     = 99
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tooling-tgt.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.tooling-tgt.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["tooling.bossladies.click"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["tooling.bossladies.click"]
+#     }
+#   }
+# }
+
 
 
