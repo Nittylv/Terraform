9e38e867a9f299db484a2051ef8d546bd4925d25
Ubuntu <ubuntu@ip-172-31-19-147.us-east-2.compute.internal>
2022-11-23 20:20:14 +0000

    updates

--- providers.tf
+++ providers.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key=var.aws_access_key
-  secret_key=var.aws_secret_key
+  access_key= var.aws_access_key
+  secret_key= var.aws_secret_key
 }
